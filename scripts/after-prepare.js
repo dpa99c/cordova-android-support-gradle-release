2f7e0eec50e99a9d665c6d289b9a7f431ba4cb48
Dave Alden <dpa99c@gmail.com>
2018-03-29 11:00:18 +0100

    Rework scripts to use promises to enforce synchronous running and therefore read/write access to gradle files to prevent concurrency issues. See https://github.com/dpa99c/cordova-android-play-services-gradle-release/issues/8

--- scripts/after-prepare.js
+++ scripts/after-prepare.js
@@ -1,60 +1,63 @@
-const PLUGIN_NAME         = "cordova-android-support-gradle-release";
+const PLUGIN_NAME = "cordova-android-support-gradle-release";
+const SCRIPT_NAME = "after-prepare";
+var deferral;
 
-try{
-    var fs = require('fs');
-    var path = require('path');
-    var parser = require('xml2js');
-}catch(e){
-    throw PLUGIN_NAME + ": Failed to load dependencies. If using cordova@6 CLI, ensure this plugin is installed with the --fetch option: " + e.message;
+function log(message) {
+    console.log(PLUGIN_NAME + "." + SCRIPT_NAME + ": " + message);
 }
 
-const GRADLE_FILENAME = path.resolve(process.cwd(), 'platforms', 'android', 'build.gradle');
-const PACKAGE_PATTERN = /(compile "com.android.support:[^:]+:)([^"]+)"/;
+function onError(error) {
+    log("ERROR: " + error);
+    deferral.resolve();
+}
 
-// 1. Parse cordova.xml file and fetch this plugin's <variable name="ANDROID_SUPPORT_VERSION" />
-fs.readFile(path.resolve(process.cwd(), 'config.xml'), function (err, data) {
-    var json = parser.parseString(data, function (err, result) {
-        if (err) {
-            return console.log(PLUGIN_NAME, ": ERROR: ", err);
-        }
-        var plugins = result.widget.plugin;
-        if(!plugins || plugins.length === 0) return;
+function run() {
+    try {
+        var fs = require('fs');
+        var path = require('path');
+        var parser = require('xml2js');
+    } catch (e) {
+        throw("Failed to load dependencies. If using cordova@6 CLI, ensure this plugin is installed with the --fetch option: " + e.toString());
+    }
+
+    const GRADLE_FILENAME = path.resolve(process.cwd(), 'platforms', 'android', 'build.gradle');
+    const PACKAGE_PATTERN = /(compile "com.android.support:[^:]+:)([^"]+)"/;
+
+    var data = fs.readFileSync(path.resolve(process.cwd(), 'config.xml'));
+    parser.parseString(data, attempt(function (err, result) {
+        if (err) throw err;
+        var version, plugins = result.widget.plugin;
         for (var n = 0, len = plugins.length; n < len; n++) {
             var plugin = plugins[n];
-            if (plugin.$.name === PLUGIN_NAME) {
-                if (!plugin.variable || plugin.variable.length === 0) {
-                    return console.log(PLUGIN_NAME, ' Failed to find <variable name="ANDROID_SUPPORT_VERSION" /> in config.xml');
-                }
-                // 2.  Update .gradle file.
-                setGradleVersion(plugin.variable.pop().$.value);
+            if (plugin.$.name === PLUGIN_NAME && plugin.variable && plugin.variable.length > 0) {
+                version = plugin.variable.pop().$.value;
                 break;
             }
         }
-    });
-});
-
-/**
- * Write properties.gradle with:
- *
- ext {
-  ANDROID_SUPPORT_VERSION = '<VERSION>'
-}
- *
- */
-function setGradleVersion(version) {
-    fs.readFile(GRADLE_FILENAME, function (err, contents) {
-        if (err) {
-            return console.log(PLUGIN_NAME, " ERROR: ", err);
+        if (version) {
+            var contents = fs.readFileSync(GRADLE_FILENAME).toString();
+            fs.writeFileSync(GRADLE_FILENAME, contents.replace(PACKAGE_PATTERN, "$1" + version + '"'), 'utf8');
+            log("Wrote custom version '" + version + "' to " + GRADLE_FILENAME);
+        } else {
+            log("No custom version found in config.xml - using plugin default");
         }
-        contents = contents.toString();
-        fs.writeFile(GRADLE_FILENAME, contents.replace(PACKAGE_PATTERN, "$1" + version + '"'), 'utf8', function (err) {
-            if (err) return console.log(PLUGIN_NAME, ": FAILED TO WRITE ", GRADLE_FILENAME, " > ", version, err);
-            console.log(PLUGIN_NAME, ": WROTE ", GRADLE_FILENAME, " > ", version);
-        });
-    });
+        deferral.resolve();
+    }));
+
 }
 
+function attempt(fn) {
+    return function () {
+        try {
+            fn.apply(this, arguments);
+        } catch (e) {
+            onError("EXCEPTION: " + e.toString());
+        }
+    }
+}
 
-
-
-
+module.exports = function (ctx) {
+    deferral = ctx.requireCordovaModule('q').defer();
+    attempt(run)();
+    return deferral.promise;
+};
--- scripts/before-prepare.js
+++ scripts/before-prepare.js
@@ -1,54 +1,62 @@
-const PLUGIN_NAME         = "cordova-android-support-gradle-release";
+const PLUGIN_NAME = "cordova-android-support-gradle-release";
+const SCRIPT_NAME = "before-prepare";
+var deferral;
 
-try{
-    var fs = require('fs');
-    var path = require('path');
-    var parser = require('xml2js');
-}catch(e){
-    throw PLUGIN_NAME + ": Failed to load dependencies. If using cordova@6 CLI, ensure this plugin is installed with the --fetch option: " + e.message;
+function log(message) {
+    console.log(PLUGIN_NAME + "." + SCRIPT_NAME + ": " + message);
 }
 
-const GRADLE_FILENAME     = path.resolve(process.cwd(), 'platforms', 'android', PLUGIN_NAME, 'properties.gradle');
-const PROPERTIES_TEMPLATE = 'ext {ANDROID_SUPPORT_VERSION = "<VERSION>"}'
+function onError(error) {
+    log("ERROR: " + error);
+    deferral.resolve();
+}
 
-// 1. Parse cordova.xml file and fetch this plugin's <variable name="ANDROID_SUPPORT_VERSION" />
-fs.readFile(path.resolve(process.cwd(), 'config.xml'), function(err, data) {
-  var json = parser.parseString(data, function(err, result) {
-    if (err) {
-      return console.log(PLUGIN_NAME, " ERROR: ", err);
+function run() {
+    try {
+        var fs = require('fs');
+        var path = require('path');
+        var parser = require('xml2js');
+    } catch (e) {
+        throw("Failed to load dependencies. If using cordova@6 CLI, ensure this plugin is installed with the --fetch option: " + e.toString());
     }
-    var plugins = result.widget.plugin;
-    if(!plugins || plugins.length === 0) return;
-    for (var n=0,len=plugins.length;n<len;n++) {
-      var plugin = plugins[n];
-      if (plugin.$.name === PLUGIN_NAME) {
-        if (!plugin.variable || plugin.variable.length === 0) {
-          return console.log(PLUGIN_NAME, ' Failed to find <variable name="ANDROID_SUPPORT_VERSION" /> in config.xml');
+
+    const GRADLE_FILENAME = path.resolve(process.cwd(), 'platforms', 'android', PLUGIN_NAME, 'properties.gradle');
+    const PROPERTIES_TEMPLATE = 'ext {ANDROID_SUPPORT_VERSION = "<VERSION>"}';
+
+    var data = fs.readFileSync(path.resolve(process.cwd(), 'config.xml'));
+    parser.parseString(data, attempt(function (err, result) {
+        if (err) throw err;
+        var version, plugins = result.widget.plugin;
+        for (var n = 0, len = plugins.length; n < len; n++) {
+            var plugin = plugins[n];
+            if (plugin.$.name === PLUGIN_NAME && plugin.variable && plugin.variable.length > 0) {
+                version = plugin.variable.pop().$.value;
+                break;
+            }
         }
-        // 2.  Update .gradle file.
-        setGradleVersion(plugin.variable.pop().$.value);
-        break;
-      }
+        if (version) {
+            fs.writeFileSync(GRADLE_FILENAME, PROPERTIES_TEMPLATE.replace(/<VERSION>/, version), 'utf8');
+            log("Wrote custom version '" + version + "' to " + GRADLE_FILENAME);
+        } else {
+            log("No custom version found in config.xml - using plugin default");
+        }
+        deferral.resolve();
+    }));
+
+}
+
+function attempt(fn) {
+    return function () {
+        try {
+            fn.apply(this, arguments);
+        } catch (e) {
+            onError("EXCEPTION: " + e.toString());
+        }
     }
-  });
-});
-
-/**
-* Write properties.gradle with:
-*
-ext {
-  ANDROID_SUPPORT_VERSION = '<VERSION>'
-}
-*
-*/
-function setGradleVersion(version) {
-  console.log(PLUGIN_NAME, " ANDROID_SUPPORT_VERSION: ", version);
-  fs.writeFile(GRADLE_FILENAME, PROPERTIES_TEMPLATE.replace(/<VERSION>/, version), 'utf8', function (err) {
-     if (err) return console.log(PLUGIN_NAME, " FAILED TO WRITE ", GRADLE_FILENAME, " > ", version, err);
-  });
 }
 
-
-
-
-
+module.exports = function (ctx) {
+    deferral = ctx.requireCordovaModule('q').defer();
+    attempt(run)();
+    return deferral.promise;
+};
